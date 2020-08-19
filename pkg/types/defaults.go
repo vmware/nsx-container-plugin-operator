731d6fb92153e3ad3d88045bb0df62ba3ff8a087
Danting Liu <dantingl@vmware.com>
2020-08-19 03:48:01 -0700

    Move certificates to secrets

--- pkg/types/defaults.go
+++ pkg/types/defaults.go
@@ -9,4 +9,4 @@ const (
 
 var NcpSections = []string{"DEFAULT", "ha", "k8s", "coe", "nsx_v3", "vc"}
 var AgentSections = []string{"DEFAULT", "k8s", "coe", "nsx_node_agent", "nsx_kube_proxy"}
-var OperatorSections = []string{"DEFAULT", "ha", "k8s", "coe", "nsx_v3", "vc", "nsx_node_agent", "nsx_kube_proxy", "operator"}
+var OperatorSections = []string{"DEFAULT", "ha", "k8s", "coe", "nsx_v3", "vc", "nsx_node_agent", "nsx_kube_proxy"}
